e45b4deb64cfdecb30697ec95b255e88355e6f32
Abhishek Hazra <enigma.abhi.ty@gmail.com>
2025-11-21 13:35:13 +0530

    Vendors and Teams Ui update

--- src/Pages/Team/Teams.js
+++ src/Pages/Team/Teams.js
@@ -1,18 +1,88 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import Header from "../../Components/Header/Header";
+import VendorsTable from "../../Components/Tables/VendorsTable";
+import TeamsTable from "../../Components/Tables/TeamsTable";
+import FloatingLabelInput from "../../Components/FloatingInput/FloatingLabelInput";
 
-const Teams=()=>{
-    return(
-        <div className="main">
-            <div className="mainContainer">
-                <Sidebar page="Teams" />
-                <div className="mainPage">
-                    <Header heading="Teams" />
+const Teams = () => {
+  const [modal, setModal] = useState(false);
+  const teamList = [
+    {
+      id: 1,
+      teamName: "Abhishek",
+      teamPhone: "9007453398",
+      teamAlternative: "7980651358",
+      teamEmail: "abhimicro3@gmail.com",
+      teamTasksDue: "10",
+    },
+  ];
+  return (
+    <div className="main">
+      <div className="mainContainer">
+        <Sidebar page="Teams" />
+        <div className="mainPage">
+          <Header heading="My Team" />
+          <div className="mainContent">
+            <div className="eventTabsSection">
+              <div className="eventTabButtons">
+                <div className="eventTabButton tabSelected">
+                  <p>Team List</p>
                 </div>
+              </div>
+
+              <div className="eventCreateSection">
+                <button onClick={() => setModal(true)}>Add Member</button>
+              </div>
             </div>
+            {teamList.length > 0 && (
+              <div className="eventCardSection">
+                <TeamsTable teams={teamList} />
+              </div>
+            )}
+          </div>
         </div>
-    )
-}
+      </div>
+      {modal && <TeamModal setModal={setModal} />}
+    </div>
+  );
+};
 
 export default Teams;
+
+const TeamModal = ({ setModal }) => {
+  return (
+    <div className="pageModal">
+      <div className="modal">
+        <img
+          src="assets/common/close.png"
+          className="modalClose"
+          onClick={() => setModal(false)}
+        />
+        <div className="modalHeader">
+          <p>Add Member</p>
+        </div>
+        <div className="modalUnit">
+          <FloatingLabelInput label="Member Name" />
+        </div>
+        <div className="modalUnit">
+          <div className="modalUnitHalf">
+            <FloatingLabelInput label="Phone" />
+          </div>
+          <div className="modalUnitHalf">
+            <FloatingLabelInput label="Email" />
+          </div>
+        </div>
+        <div className="modalUnit">
+          <div className="modalUnitHalf">
+            <FloatingLabelInput label="Alternative Number" />
+          </div>
+        </div>
+        <div className="modalButtonSection">
+          <button>Cancel</button>
+          <button>Create</button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/Pages/Vendors/Vendors.js
+++ src/Pages/Vendors/Vendors.js
@@ -1,21 +1,96 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import Header from "../../Components/Header/Header";
+import VendorsTable from "../../Components/Tables/VendorsTable";
+import FloatingSelectInput from "../../Components/FloatingInput/FloatingSelectInput";
+import FloatingLabelInput from "../../Components/FloatingInput/FloatingLabelInput";
 
-const Vendors=()=>{
-    return(
-        <div className="main">
-            <div className="mainContainer">
-                <Sidebar page="Vendors" />
-                <div className="mainPage">
-                    <Header heading="Vendors" />
-                    <div className="mainContent">
-                        
-                    </div>
+const Vendors = () => {
+  const [modal,setModal]=useState(false)
+  const vendorList = [
+    {
+      id: 1,
+      vendorName: "Abhishek",
+      vendorPhone: "9007453398",
+      vendorAlternative: "7980651358",
+      vendorEmail: "abhimicro3@gmail.com",
+      vendorCategory: "Catering",
+      vendorTasksDue:"10"
+    },
+  ];
+  return (
+    <div className="main">
+      <div className="mainContainer">
+        <Sidebar page="Vendors" />
+        <div className="mainPage">
+          <Header heading="Vendors" />
+          <div className="mainContent">
+            <div className="eventTabsSection">
+              <div className="eventTabButtons">
+                <div
+                  className="eventTabButton tabSelected" 
+                >
+                  <p>Vendors List</p>
+                </div>
+              </div>
+
+                <div className="eventCreateSection">
+                  <button onClick={() => setModal(true)}>Add Vendor</button>
                 </div>
             </div>
+            {
+            vendorList.length > 0 && (
+              <div className="eventCardSection">
+                <VendorsTable vendors={vendorList}/>
+              </div>
+            )
+           }
+          </div>
         </div>
-    )
-}
+      </div>
+      {modal && <VendorModal setModal={setModal}/>}
+    </div>
+  );
+};
 
 export default Vendors;
+
+const VendorModal = ({ setModal }) => {
+    return (
+      <div className="pageModal">
+        <div className="modal">
+          <img
+            src="assets/common/close.png"
+            className="modalClose"
+            onClick={() => setModal(false)}
+          />
+          <div className="modalHeader">
+            <p>Add Vendor</p>
+          </div>
+          <div className="modalUnit">
+            <FloatingLabelInput label="Vendor Name" />
+          </div>
+          <div className="modalUnit">
+            <div className="modalUnitHalf">
+              <FloatingLabelInput label="Phone" />
+            </div>
+            <div className="modalUnitHalf">
+              <FloatingLabelInput label="Alternative Number" />
+            </div>
+          </div>
+          <div className="modalUnit">
+            <div className="modalUnitHalf">
+              <FloatingLabelInput label="Email" />
+            </div>
+            <div className="modalUnitHalf">
+              <FloatingSelectInput label="Vendor Category"/>
+            </div>
+          </div>
+          <div className="modalButtonSection">
+            <button>Cancel</button>
+            <button>Create</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
